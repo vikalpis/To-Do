6b43cbef047fa265c7c15d6257f43d6dc79b29db
Vikalp Pandey <149229170+vikalpis@users.noreply.github.com>
2025-02-06 15:05:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,9 +74,10 @@ import './App.css'
 // import { Todoitem } from './Components/Todoitem'
 import { Todoform } from './Components'
 import {Todoitem} from './Components'
-
+import TodoReminder from './TodoRemainder'
 function App() {
   const [todos, setTodos] = useState([])
+  const [time, setTime] = useState("")
 
   const addTodo = (todo) => {
     setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
@@ -112,32 +113,40 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
   
+// here
+const updatetodoTime = (id, newTime) => {
+  setTodos((prevtodos) => prevtodos.map((todo) => (todo.id === id ? { ...todo, time: newTime } : todo)))
+}
 
-
-
-  return (
-    <Todoprovider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">
-                        {/* Todo form goes here */} 
-                        <Todoform />
-                    </div>
-                    <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add Todoitem here */}
-                        {todos.map((todo) => (
-                          <div key={todo.id}
-                          className='w-full'
-                          >
-                            <Todoitem todo={todo} />
-                          </div>
-                        ))}
-                    </div>
-                </div>
+return (
+  <Todoprovider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+    <div className="bg-[#172842] min-h-screen py-8">
+      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+          <Todoform />
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+          {todos.map((todo) => (
+            <div key={todo.id} className="w-full">
+              <Todoitem todo={todo} />
+              <div className="p-2 sm:p-1">
+                <input
+                  type="datetime-local"
+                  value={todo.time || ""}
+                  onChange={(e) => updatetodoTime(todo.id, e.target.value)}
+                  className="border bg-pink-200 text-black p-1 rounded w-full mt-1"
+                />
+                <TodoReminder todo={todo.todo} time={todo.time} />
+              </div>
             </div>
-    </Todoprovider>
-  )
+          ))}
+        </div>
+      </div>
+    </div>
+  </Todoprovider>
+)
 }
 
 export default App
+

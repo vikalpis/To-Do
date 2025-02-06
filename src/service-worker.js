1b2b157a590c77a96f35069e3eb789f3e0b94444
Vikalp Pandey <149229170+vikalpis@users.noreply.github.com>
2025-02-06 15:37:08 +0530

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,24 @@
-self.addEventListener("message", (event) => {
-    const data = event.data;
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: "notification-icon.png",
-    });
-});
+self.addEventListener("install", (event) => {
+    console.log("Service Worker installed")
+  })
+  
+  self.addEventListener("activate", (event) => {
+    console.log("Service Worker activated")
+  })
+  
+  self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SCHEDULE_NOTIFICATION") {
+      const { todo, time } = event.data
+      const delay = new Date(time) - new Date()
+  
+      if (delay > 0) {
+        setTimeout(() => {
+          self.registration.showNotification("Reminder!", {
+            body: `Time to complete: ${todo}`,
+            icon: "/notification-icon.png",
+          })
+        }, delay)
+      }
+    }
+  })
+  

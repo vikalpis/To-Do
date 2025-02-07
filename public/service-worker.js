d1bca3232111b89809c39b26e74c0bddb4c5bc8a
Vikalp Pandey <149229170+vikalpis@users.noreply.github.com>
2025-02-07 14:56:04 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker installed")
+  })
+  
+  self.addEventListener("activate", (event) => {
+    console.log("Service Worker activated")
+  })
+  
+  self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SCHEDULE_NOTIFICATION") {
+      const { todo, time } = event.data
+      const delay = new Date(time) - new Date()
+  
+      if (delay > 0) {
+        setTimeout(() => {
+          self.registration.showNotification("Reminder!", {
+            body: `Time to complete: ${todo}`,
+            icon: "/notification-icon.png",
+          })
+        }, delay)
+      }
+    }
+  })
+  
+  
